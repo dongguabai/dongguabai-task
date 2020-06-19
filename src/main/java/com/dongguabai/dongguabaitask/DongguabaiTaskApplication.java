1dfdb93465dd27f7e8f57f0f2d5e056fc82d9c2c
魏民 <weimin@xiaomi.com>
2020-06-19 23:55:31 +0800

    feat: init

---
+++ src/main/java/com/dongguabai/dongguabaitask/DongguabaiTaskApplication.java
@@ -0,0 +1,13 @@
+package com.dongguabai.dongguabaitask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DongguabaiTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DongguabaiTaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dongguabai/dongguabaitask/DongguabaiTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dongguabai.dongguabaitask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DongguabaiTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
